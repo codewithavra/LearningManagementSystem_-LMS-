7bd2e0c14f8584b9b871c4aea7af0dbf176f3922
codewithavra <244584119+codewithavra@users.noreply.github.com>
2026-03-22 22:12:31 +0530

    Add security and performance enhancements: integrate CORS, rate limiting, and sanitization middleware; update dependencies in package.json and package-lock.json

--- server/src/ENV/index.js
+++ server/src/ENV/index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
-
 export const ENV = {
     PORT : process.env.PORT,
-    NODE_ENV : process.env.NODE_ENV
+    NODE_ENV : process.env.NODE_ENV,
+    CLIENT_URL:process.env.CLIENT_URL
 }
--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,11 @@
  */
 import express from "express";
 import morgan from "morgan";
-
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
+import cors from "cors";
 /**
  * other modules
  */
@@ -14,6 +18,27 @@ import { ENV } from "./ENV/index.js";
  */
 const app = express();
 
+/**
+ * @description Global rate limiting
+ */
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+
+// Apply the rate limiting middleware to all requests.
+
+/**
+ * @description security middleware
+ */
+app.use(helmet());
+app.use(ExpressMongoSanitize());
+app.use('/api',limiter);
+app.use(hpp());
 /**
  * @description logging middleware
  */
@@ -28,22 +53,50 @@ if(ENV.NODE_ENV === "development"){
 app.use(express.json({limit : '10kb'}))
 app.use(express.urlencoded({extended : true, limit : '10kb'}))
 
+
+
 /**
  * @description Global error handler
  */
-app.use((err,req,res,next)=>{
-    console.error(err.stack);
-    res.status(err.status || 500).json({
-        status : "error",
-        message : err.message || 'Internal Server error',
-        ...(ENV.NODE_ENV === 'development' && {stack : err.stack})
-    })
+app.use((err, req,res,next)=>{
+    const isDev = (ENV.NODE_ENV === 'development');
+    const statusCode = err.status || err.statusCode || 500;
     
+    if(isDev){
+        console.error(err.stack);
+    }else{
+        console.error(err.message);
+    }
+
+    res.status(statusCode).json({
+        status : "error",
+        message : err.message || 'Internal Server Error',
+        ...(isDev && ({stack : err.stack}))
+    })
 })
+
+/**
+ * @description CORS configuration
+ */
+app.use(cors({
+    origin: ENV.CLIENT_URL || "http://lovalhost:5173",
+    credentials : true,
+    methods : ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"  ],
+    allowedHeaders : [
+        "Content-Type", 
+        "Authorization", 
+        "X-Requested-With", 
+        "device-remember-token",
+        "Access-Control-Allow-Origin",
+        "Accept", 
+        "Origin", ],
+}))
+
 
 /**
  * @description API routes
  */
+
 
 
 /**
